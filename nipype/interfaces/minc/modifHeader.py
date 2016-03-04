46ea7da01fff7b64aa2e0dba134bc949966a3e25
root <kevin.mh.larcher@gmail.com>
2016-03-03 23:30:05 -0500

    Update

--- nipype/interfaces/minc/modifHeader.py
+++ nipype/interfaces/minc/modifHeader.py
@@ -16,13 +16,14 @@ class ModifyHeaderOutput(TraitedSpec):
 class ModifyHeaderInput(BaseInterfaceInputSpec):
     in_file = File(position=-1, argstr="%s", mandatory=True, desc="Image")
 
-    opt_string = traits.Str(argstr="%s", mandatory=True, desc="Option defining the infos to print out")
     sinsert = traits.Bool(argstr="-sinsert", usedefault=True, default_value=True, desc="Insert a string attribute")
     dinsert = traits.Bool(argstr="-dinsert", usedefault=True, default_value=True, desc="Insert a double precision attribute")
     sappend = traits.Bool(argstr="-sappend", usedefault=True, default_value=True, desc="Append a string attribute")
-    dappend = traits.Bool(argstr="-dappend", usedefault=True, default_value=True, desc="Insert a double precision attribute")
-    delete = traits.Bool(argstr="-delete", usedefault=True, default_value=True, desc="Delete an attribute")
+    dappend = traits.Bool(argstr="-dappend", usedefault=True, default_value=True, desc="Append a double precision attribute")
+    opt_string = traits.Str(argstr="%s", mandatory=True, desc="Option defining the infos to print out")
 
+    delete = traits.Bool(argstr="-delete", usedefault=True, default_value=True, desc="Delete an attribute")
+ 
     clobber = traits.Bool(argstr="-clobber", usedefault=True, default_value=True, desc="Overwrite output file")
     verbose = traits.Bool(argstr="-verbose", usedefault=True, default_value=True, desc="Write messages indicating progress")
 

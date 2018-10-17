0b69f3553291188c2f10e6d3bceb3cc2b78cbcb1
tf <thomas.funck@mail.mcgill.ca>
2018-10-17 10:34:56 -0400

    in Launcher.py changed dirname to abspath in order to get path to file

--- Test/test.sh
+++ Test/test.sh
@@ -3,8 +3,9 @@ SCRIPTPATH="$( cd "$(dirname "$0")"; cd .. ; pwd -P )"
 i=0
 
 #zu
-
-docker run  -v "$SCRIPTPATH":"/APPIAN" tffunck/appian:latest bash -c "python /APPIAN/Launcher.py  -s /APPIAN/Test/test_gabriel -t /APPIAN/Test/out_test_zu --pvc-fwhm 3 3 3 --sessions zu s002"
+docker_path="/opt/APPIAN/Launcher.py"
+#docker_path="/data/Launcher.py"
+docker run  -v "$SCRIPTPATH":"/data" tffunck/appian:latest bash -c "python $docker_path  -s /data/Test/test_gabriel -t /data/Test/out_test_zu --pvc-fwhm 3 3 3 --sessions zu s002"
 
 #CIMBI
 #docker run -v "$SCRIPTPATH":"/APPIAN" tffunck/appian:latest bash -c "python2.7 /APPIAN/Launcher.py --no-pvc --user-brainmask --pvc-label-space stereo  --pvc-label-img antsAtropos  --tka-label-space stereo --tka-method 'lp' --tka-label-img antsAtropos --tka-labels-brain-only --tka-labels-ones-only  --tka-label 3 --tka-label-erosion 5 --results-label-space stereo  --results-labels-brain-only --results-label 2 --results-label-img antsAtropos --sourcedir /APPIAN/Test/test_data --targetdir /APPIAN/Test/out_test_cimbi  --sessions 01  --fwhm 3 3 3  01";

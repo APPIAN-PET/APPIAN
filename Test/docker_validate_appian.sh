f35037b9c12988581586c4b0867ae6b7e7eca683
tf <thomas.funck@mail.mcgill.ca>
2019-01-03 11:46:11 -0500

    added qc option for validation, also for docker_validate_appian.sh script

--- Test/docker_validate_appian.sh
+++ Test/docker_validate_appian.sh
@@ -1,3 +1,3 @@
 SCRIPTPATH="$( cd "$(dirname "$0")"; cd .. ; pwd -P )"
 
-docker run -v "$SCRIPTPATH":"/APPIAN" tffunck/appian:latest bash -c "/APPIAN/Test/validate_appian.sh $1 $2 $3 $4 $5 $6"
+docker run -v "$SCRIPTPATH":"/APPIAN" tffunck/appian:latest bash -c "/APPIAN/Test/validate_appian.sh $1 $2 $3 $4 $5 $6 $7"

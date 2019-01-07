a9920d803c62f939da53d051a4ea0f5d2645fbd8
tfunck <thomas.funck@mail.mcgill.ca>
2019-01-07 17:37:44 +0000

    added missing pvc method

--- Test/docker_validate_appian.sh
+++ Test/docker_validate_appian.sh
@@ -1,3 +1,14 @@
 SCRIPTPATH="$( cd "$(dirname "$0")"; cd .. ; pwd -P )"
 
-docker run -v "$SCRIPTPATH":"/APPIAN" tffunck/appian:latest bash -c "/APPIAN/Test/validate_appian.sh $1 $2 $3 $4 $5 $6 $7"
+threads=${1:-1} 
+base_path=${2:-"/APPIAN"}
+test_data_path=${3:-"/APPIAN/Test/test_data"}
+out_data_path=${4:-"/APPIAN/Test/"}
+#Create a timestamp for testing session. 
+#Can use existing timestamp if using output files from previous test run
+exit_on_failure=${5:-0}
+qc=${6:-0}
+ts=${7:-`date +"%Y%m%d-%H%M%S"`}
+test_dir="${out_data_path}/appian_validation/test_$ts"
+
+docker run -v "$SCRIPTPATH":"/APPIAN" tffunck/appian:latest bash -c "/APPIAN/Test/validate_appian.sh $threads $base_path $test_data_path $out_data_path $exit_on_failure $qc $ts $test_dir"
